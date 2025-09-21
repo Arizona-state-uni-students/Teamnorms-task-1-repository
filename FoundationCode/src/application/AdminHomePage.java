56257b29fa6f88e7da44bc55c7f09c9cd09db01b
Max Gushchin <maxgushchin1@gmail.com>
2025-09-21 16:07:09 -0700

    Refactor AdminHomePage and add PasswordEvaluator class

--- FoundationCode/src/application/AdminHomePage.java
+++ FoundationCode/src/application/AdminHomePage.java
@@ -1,107 +1,239 @@
 package application;
 
-import java.sql.SQLException;
 
-import databasePart1.DatabaseHelper;
-import javafx.application.Platform;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
-
-/**
- * AdminPage class represents the user interface for the admin user.
- * This page displays a simple welcome message for the admin.
- */
-
-public class AdminHomePage {
+public class PasswordEvaluator {
 	/**
-     * Displays the admin page in the provided primary stage.
-     * @param primaryStage The primary stage where the scene will be displayed.
-     */
-	// Reference to the DatabaseHelper for database interactions
-	private final DatabaseHelper databaseHelper;
-	public AdminHomePage(DatabaseHelper databaseHelper) {
-        this.databaseHelper = databaseHelper;
-    }
-    public void show(Stage primaryStage, User user) {
-    	// Establish GUI Grid
-    	GridPane grid = new GridPane();
-    	//grid.setGridLinesVisible(true); // for testing
-    	grid.setPadding(new Insets(10));
-    	grid.setHgap(10); // Horizontal gap between columns
-    	grid.setVgap(10); // Vertical gap between rows
-    	grid.setAlignment(javafx.geometry.Pos.TOP_CENTER);
-    	
-    	// set background image
-    	Image backgroundImage = new Image(getClass().getResource("/blankadmin.png").toExternalForm());
-    	BackgroundImage backgroundImg = new BackgroundImage(
-    			backgroundImage,
-    			BackgroundRepeat.NO_REPEAT,
-    			BackgroundRepeat.NO_REPEAT,
-    			BackgroundPosition.CENTER,
-    			new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, true, true, true, true)
-    			);
-    	grid.setBackground(new Background(backgroundImg));
-    	
-	    Label adminLabel = new Label("Hello, Admin!");
-	    adminLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
-	    
-	    
-	    Button truncateButton = new Button("Truncate Database");
-	    truncateButton.setOnAction(a -> {
-	    	databaseHelper.truncate();
-	    	databaseHelper.closeConnection();
-	    	Platform.exit();
-	    });
-	    
-	    // Button directory to traverse into the User Database Page
-	    Button userDatabase = new Button("User Database");
-	    userDatabase.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #ff9900; -fx-text-fill: black;");
-	    userDatabase.setOnAction(a -> {
-	    		try {
-					new UserDatabaseUI(databaseHelper, user).show(primaryStage);
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block. Need to adjust
-					e.printStackTrace();
-				}
-	    });	    
-	    
-        Button inviteButton = new Button("Generate Invitations");
-        inviteButton.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #ff9900; -fx-text-fill: black;");
-        inviteButton.setOnAction(a -> {
-                new InvitationPage().show(databaseHelper, primaryStage, user);
-        });
-        
-	    Button logoutButton = new Button("Logout");
-	    logoutButton.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #666; -fx-text-fill: white;");
-	    logoutButton.setOnAction(a -> {
-	            new UserLoginPage(databaseHelper).show(primaryStage);
-	    });
-	    Button switchRoleButton = new Button("Pick Role"); 
-	    switchRoleButton.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #0099ff; -fx-text-fill: white;");
-	    switchRoleButton.setOnAction(a -> {
-	    	new WelcomeLoginPage(databaseHelper).show(primaryStage,user);
-	    });
+	 * <p> Title: Directed Graph-translated Password Assessor. </p>
+	 * 
+	 * <p> Description: A demonstration of the mechanical translation of Directed Graph 
+	 * diagram into an executable Java program using the Password Evaluator Directed Graph. 
+	 * The code detailed design is based on a while loop with a cascade of if statements</p>
+	 * 
+	 * <p> Copyright: Lynn Robert Carter © 2022 </p>
+	 * 
+	 * @author Lynn Robert Carter
+	 * 
+	 * @version 0.00		2018-02-22	Initial baseline 
+	 * 
+	 */
 
-	    grid.add(adminLabel, 0, 0);    // Column 1, Row 0
-        grid.add(userDatabase, 0, 1);     // Column 1, Row 1
-	    grid.add(inviteButton, 0, 2); // Column 0, Row 2
-        grid.add(switchRoleButton, 0, 3); // Column 0, Row 3
-        grid.add(logoutButton, 0, 4);
-	    
-	    
-	    Scene adminScene = new Scene(grid, 800, 400);
-	    primaryStage.setScene(adminScene);
-	    primaryStage.setTitle("Admin Page");
-    }
+	/**********************************************************************************************
+	 * 
+	 * Result attributes to be used for GUI applications where a detailed error message and a 
+	 * pointer to the character of the error will enhance the user experience.
+	 * 
+	 */
+
+	public static String passwordErrorMessage = "";		// The error message text
+	public static String passwordInput = "";			// The input being processed
+	public static int passwordIndexofError = -1;		// The index where the error was located
+	public static boolean PfoundUpperCase = false;
+	public static boolean PfoundLowerCase = false;
+	public static boolean PfoundNumericDigit = false;
+	public static boolean PfoundSpecialChar = false;
+	public static boolean PfoundLongEnough = false;
+	public static boolean PfoundOtherChar = false;
+	public static boolean UfoundUpperCase = false;
+	public static boolean UfoundLowerCase = false;
+	public static boolean UfoundNumericDigit = false;
+	public static boolean UfoundSpecialChar = false;
+	public static boolean UfoundLongEnough = false;
+	public static boolean UfoundOtherChar = false;
+	private static String inputLine = "";				// The input line
+	private static char currentChar;					// The current character in the line
+	private static int currentCharNdx;					// The index of the current character
+	private static boolean running;						// The flag that specifies if the FSM is 
+														// running
+
+	/**********
+	 * This private method display the input line and then on a line under it displays an up arrow
+	 * at the point where an error should one be detected.  This method is designed to be used to 
+	 * display the error message on the console terminal.
+	 * 
+	 * @param input				The input string
+	 * @param currentCharNdx	The location where an error was found
+	 * @return					Two lines, the entire input line followed by a line with an up arrow
+	 */
+//	private static void displayInputState() {
+//		// Display the entire input line
+//		System.out.println(inputLine);
+//		System.out.println(inputLine.substring(0,currentCharNdx) + "?");
+//		System.out.println("The password size: " + inputLine.length() + "  |  The currentCharNdx: " + 
+//				currentCharNdx + "  |  The currentChar: \"" + currentChar + "\"");
+//	}
+
+	/**********
+	 * This method is a mechanical transformation of a Directed Graph diagram into a Java
+	 * method.
+	 * 
+	 * @param input		The input string for directed graph processing
+	 * @return			An output string that is empty if every things is okay or it will be
+	 * 						a string with a help description of the error follow by two lines
+	 * 						that shows the input line follow by a line with an up arrow at the
+	 *						point where the error was found.
+	 */
+	public static String evaluatePassword(String input) {
+		// The following are the local variable used to perform the Directed Graph simulation
+		passwordErrorMessage = "";
+		passwordIndexofError = 0;			// Initialize the IndexofError
+		inputLine = input;					// Save the reference to the input line as a global
+		currentCharNdx = 0;					// The index of the current character
+		
+		if(input.length() <= 0) return "*** Error *** The password is empty!";
+		
+		// The input is not empty, so we can access the first character
+		currentChar = input.charAt(0);		// The current character from the above indexed position
+
+		// The Directed Graph simulation continues until the end of the input is reached or at some 
+		// state the current character does not match any valid transition to a next state
+
+		passwordInput = input;				// Save a copy of the input
+		PfoundUpperCase = false;				// Reset the Boolean flag
+		PfoundLowerCase = false;				// Reset the Boolean flag
+		PfoundNumericDigit = false;			// Reset the Boolean flag
+		PfoundSpecialChar = false;			// Reset the Boolean flag
+		PfoundOtherChar = false;			    // Reset the Boolean flag
+		PfoundLongEnough = false;			// Reset the Boolean flag
+		running = true;						// Start the loop
+
+		// The Directed Graph simulation continues until the end of the input is reached or at some 
+		// state the current character does not match any valid transition
+		while (running) {
+			//displayInputState();
+			// The cascading if statement sequentially tries the current character against all of the
+			// valid transitions
+			if (currentChar >= 'A' && currentChar <= 'Z') {
+				System.out.println("Upper case letter found");
+				PfoundUpperCase = true;
+			} else if (currentChar >= 'a' && currentChar <= 'z') {
+				System.out.println("Lower case letter found");
+				PfoundLowerCase = true;
+			} else if (currentChar >= '0' && currentChar <= '9') {
+				System.out.println("Digit found");
+				PfoundNumericDigit = true;
+			} else if ("~`!@#$%^&*()_-+{}[]|:,.?/".indexOf(currentChar) >= 0) { //change special characters to reflect requirements
+				System.out.println("Special character found");
+				PfoundSpecialChar = true;
+			} else {
+				passwordIndexofError = currentCharNdx;
+				PfoundOtherChar = true; //bad character identified
+				return "*** Error *** An invalid character has been found!";
+			}
+			if (currentCharNdx >= 5) {
+				System.out.println("At least 6 characters found");
+				PfoundLongEnough = true;
+			}
+			
+			// Go to the next character if there is one
+			currentCharNdx++;
+			if (currentCharNdx >= inputLine.length())
+				running = false;
+			else
+				currentChar = input.charAt(currentCharNdx);
+			
+			System.out.println();
+		}
+		
+		String errMessage = "";
+		if (!PfoundUpperCase)
+			errMessage += "Upper case; ";
+		
+		if (!PfoundLowerCase)
+			errMessage += "Lower case; ";
+		
+		if (!PfoundNumericDigit)
+			errMessage += "Numeric digits; ";
+			
+		if (!PfoundSpecialChar)
+			errMessage += "Special character; ";
+			
+		if (!PfoundLongEnough)
+			errMessage += "Long Enough; ";
+		
+		if (errMessage == "")
+			return "";
+		
+		passwordIndexofError = currentCharNdx;
+		return errMessage + "conditions were not satisfied";
+
+	}
+	
+	public static String evaluateUsername(String input) {
+		// The following are the local variable used to perform the Directed Graph simulation
+		passwordErrorMessage = "";
+		passwordIndexofError = 0;			// Initialize the IndexofError
+		inputLine = input;					// Save the reference to the input line as a global
+		currentCharNdx = 0;					// The index of the current character
+		
+		if(input.length() <= 0) return "*** Error *** The password is empty!";
+		
+		// The input is not empty, so we can access the first character
+		currentChar = input.charAt(0);		// The current character from the above indexed position
+		passwordInput = input;				// Save a copy of the input
+		UfoundUpperCase = false;				// Reset the Boolean flag
+		UfoundLowerCase = false;				// Reset the Boolean flag
+		UfoundNumericDigit = false;			// Reset the Boolean flag
+		UfoundSpecialChar = false;			// Reset the Boolean flag
+		UfoundOtherChar = false;			    // Reset the Boolean flag
+		UfoundLongEnough = false;			// Reset the Boolean flag
+		running = true;						// Start the loop
+		while (running) {
+			//displayInputState();
+			if (currentChar >= 'A' && currentChar <= 'Z') {
+				System.out.println("Upper case letter found");
+				UfoundUpperCase = true;
+			} else if (currentChar >= 'a' && currentChar <= 'z') {
+				System.out.println("Lower case letter found");
+				UfoundLowerCase = true;
+			} else if (currentChar >= '0' && currentChar <= '9') {
+				System.out.println("Digit found");
+				UfoundNumericDigit = true;
+			} else if ("~`!@#$%^&*()_-+{}[]|:,.?/ ".indexOf(currentChar) >= 0) { //change special characters to reflect requirements
+				System.out.println("Special character found");
+				UfoundSpecialChar = true;
+			} else {
+				passwordIndexofError = currentCharNdx;
+				UfoundOtherChar = true; //bad character identified
+				return "*** Error *** An invalid character has been found!";
+			}
+			if (currentCharNdx >= 3) {
+				System.out.println("At least 4 characters found");
+				UfoundLongEnough = true;
+			}
+			
+			// Go to the next character if there is one
+			currentCharNdx++;
+			if (currentCharNdx >= inputLine.length())
+				running = false;
+			else
+				currentChar = input.charAt(currentCharNdx);
+			
+			System.out.println();
+		}
+		
+		String errMessage = "";
+//		if (!UfoundUpperCase)
+//			errMessage += "Upper case; ";
+//		
+//		if (!UfoundLowerCase)
+//			errMessage += "Lower case; ";
+//		
+//		if (!UfoundNumericDigit)
+//			errMessage += "Numeric digits; ";
+//			
+		if (UfoundSpecialChar)
+			errMessage += "Special character; ";
+			
+		if (!UfoundLongEnough)
+			errMessage += "Long Enough; ";
+		
+		if (errMessage == "")
+			return "";
+		
+		passwordIndexofError = currentCharNdx;
+		return errMessage + "conditions were not satisfied";
+
+	}
 }
+
+
