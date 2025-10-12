ac330d5a08cc9d1487e21cf9aea32e5eb3f5f6a9
Luke Sherry <150623118+LukeSherry@users.noreply.github.com>
2025-10-11 17:19:56 -0700

    Update AdminHomePage.java
    Added a back button

--- FoundationCode/src/application/AdminHomePage.java
+++ FoundationCode/src/application/AdminHomePage.java
@@ -81,7 +81,16 @@ public class AdminHomePage {
         inviteButton.setOnAction(a -> {
                 new InvitationPage().show(databaseHelper, primaryStage, user);
         });
-        
+
+		// Go back button
+        Button goBackButton = new Button("Go back");
+        goBackButton.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #666; -fx-text-fill: white;");
+        // Go back action
+        goBackButton.setOnAction(e -> {
+            // Navigate back to welcome page
+            new WelcomeLoginPage(databaseHelper).show(primaryStage, user);
+        });
+		
 	    Button logoutButton = new Button("Logout");
 	    logoutButton.setStyle("-fx-font-size: 14px; -fx-padding: 5 20; -fx-background-color: #666; -fx-text-fill: white;");
 	    logoutButton.setOnAction(a -> {
@@ -97,7 +106,8 @@ public class AdminHomePage {
         grid.add(userDatabase, 0, 1);     // Column 1, Row 1
 	    grid.add(inviteButton, 0, 2); // Column 0, Row 2
         grid.add(switchRoleButton, 0, 3); // Column 0, Row 3
-        grid.add(logoutButton, 0, 4);
+		grid.add(goBackButton, 0, 4);
+        grid.add(logoutButton, 0, 5);
 	    
 	    
 	    Scene adminScene = new Scene(grid, 800, 400);
@@ -105,4 +115,5 @@ public class AdminHomePage {
 	    primaryStage.setTitle("Admin Page");
     }
 }
+
 
