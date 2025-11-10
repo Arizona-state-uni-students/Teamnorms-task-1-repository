c7c0bfddac32fca60666a46a1dfc678d487db2a2
Luke Sherry <lukes1249@gmail.com>
2025-11-09 21:38:07 -0700

    Added Javadoc HTML Files

--- FoundationCode/src/application/Question.java
+++ FoundationCode/src/application/Question.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents a question in the Q&A system.
+ * Represents a question in the Q&amp;A system.
  * Contains question content, metadata, and associated answers.
  */
 public class Question {
